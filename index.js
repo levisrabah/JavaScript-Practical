dd2122344e2e1e7b605cfdc9860e4a9fa2b82fea
LEVIS <levisrabah@gmail.com>
2024-04-01 13:19:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//performing a rask
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+//calculating a value
+function square(number){
+    return number * number;
+}
+
+console.log(square(45));
+
+greet('Sam', 'Apuruke');
+greet('Musa', 'Manuar');
+
+
